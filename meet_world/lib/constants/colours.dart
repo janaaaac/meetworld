33bc3a8a7e9a281da7b864a34292d090d7e674cb
Chamindu Janadhara <cjdissanayake@students.nsbm.ac.lk>
2025-05-25 09:05:50 +0530

    Remove leading/trailing spaces from file and folder names

---
+++ meet_world/lib/constants/colours.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color backgroundColor = Color(0xFF0A0A0A);
+  static const Color primaryColor = Color(0xFF2AFD92);
+  static const Color textColor = Color(0xFFFFFFFF);
+  static const Color cardColor = Color(0xFF1A1A1A);
+}
