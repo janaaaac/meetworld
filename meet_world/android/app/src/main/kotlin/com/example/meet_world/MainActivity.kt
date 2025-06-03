b04995e584e0be0eeb40704c21711a81cba80cde
Chamindu Janadhara <cjdissanayake@students.nsbm.ac.lk>
2025-06-03 17:10:29 +0530

    deleted

--- meet_world/android/app/src/main/kotlin/com/example/meet_world/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.meet_world
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- meet_world/lib/constants/api.dart
+++
@@ -1,2 +0,0 @@
-// const String baseUrl = 'http://10.0.2.2:5001/api'; // use 10.0.2.2 for Android emulator
-const String baseUrl = 'http://localhost:5001/api'; // for web or iOS simulator
