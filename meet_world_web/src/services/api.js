fde7c8b35249f71eda4b090471f130880a35dc37
Chamindu Janadhara <cjdissanayake@students.nsbm.ac.lk>
2025-06-03 20:08:47 +0530

    api updated

--- meet_world_web/src/services/api.js
+++ meet_world_web/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create axios instance with base URL
 const API = axios.create({
-  baseURL: 'http://localhost:5001/api',
+  baseURL: 'https://meetworldbackend-production.up.railway.app/api',
   headers: {
     'Content-Type': 'application/json',
   },
